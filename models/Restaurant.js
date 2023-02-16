6f8cf5b8cfd3ddd57889dbd5c840013f0944557d
DanielXFuller <danielxfuller@gmail.com>
2023-02-16 17:02:11 +0000

    restaurant model complete

--- models/Restaurant.js
+++ models/Restaurant.js
@@ -1,6 +1,18 @@
 const {sequelize} = require('../db');
-const { Sequelize } = require('sequelize');
+const {DataTypes} = require('sequelize');
 
-// TODO - create a Restaurant model
-
+const Restaurant = sequelize.define('Restaurant', {
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  location: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  cuisine: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
+});
 module.exports = {Restaurant};
