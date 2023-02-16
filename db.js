143682e3eb99b5eb219c79cf53018aaa9d772d90
DanielXFuller <danielxfuller@gmail.com>
2023-02-16 16:52:47 +0000

    todo for db.js

--- db.js
+++ db.js
@@ -2,6 +2,23 @@ const path = require('path');
 const { Sequelize } = require('sequelize');
 
 // TODO - connect to db via sequelize
+// Create a new Sequelize instance
+const sequelize = new Sequelize('database_name', 'username', 'password', {
+    host: 'localhost',
+    dialect: 'mysql'
+});
+  
+  // Test the connection
+async function testConnection() {
+    try {
+      await sequelize.authenticate();
+      console.log('Connection has been established successfully.');
+    } catch (error) {
+      console.error('Unable to connect to the database:', error);
+    }
+}
+  
+testConnection();
 
 module.exports = {
     sequelize
