91f5968a5647a0e4b3e221aa15f6e7cc4584b190
DanielXFuller <danielxfuller@gmail.com>
2023-02-16 17:00:10 +0000

    tests

--- index.test.js
+++ index.test.js
@@ -18,26 +18,65 @@ describe('Restaurant and Menu Models', () => {
 
     test('can create a Restaurant', async () => {
         // TODO - write test
-        expect('NO TEST').toEqual('EXPECTED DATA')
+        const restaurantData = {
+            name: 'Test Restaurant',
+            address: '123 Main St',
+            phone: '555-1234',
+        };
+        const restaurant = await Restaurant.create(restaurantData);
+  
+        expect(restaurant.name).toEqual('Test Restaurant');
+        expect(restaurant.address).toEqual('123 Main St');
+        expect(restaurant.phone).toEqual('555-1234');
     });
 
     test('can create a Menu', async () => {
         // TODO - write test
-        expect('NO TEST').toEqual('EXPECTED DATA')
+        const menuData = {
+            title: 'Test Menu',
+            RestaurantId: 1,
+        };
+        const menu = await Menu.create(menuData);
+  
+        expect(menu.title).toEqual('Test Menu');
+        expect(menu.RestaurantId).toEqual(1);
     });
 
     test('can find Restaurants', async () => {
         // TODO - write test
-        expect('NO TEST').toEqual('EXPECTED DATA')
+        await seedRestaurant();
+
+        const restaurants = await Restaurant.findAll();
+  
+        expect(restaurants.length).toEqual(2);
+        expect(restaurants[0].name).toEqual('Restaurant A');
+        expect(restaurants[1].name).toEqual('Restaurant B');
     });
 
     test('can find Menus', async () => {
         // TODO - write test
-        expect('NO TEST').toEqual('EXPECTED DATA')
+        await seedMenu();
+
+        const menus = await Menu.findAll();
+  
+        expect(menus.length).toEqual(2);
+        expect(menus[0].title).toEqual('Menu A');
+        expect(menus[1].title).toEqual('Menu B');
     });
 
     test('can delete Restaurants', async () => {
         // TODO - write test
-        expect('NO TEST').toEqual('EXPECTED DATA')
+        await seedRestaurant();
+
+        const restaurantsBefore = await Restaurant.findAll();
+        expect(restaurantsBefore.length).toEqual(2);
+  
+        await Restaurant.destroy({
+            where: { name: 'Restaurant A' }
+        });
+  
+        const restaurantsAfter = await Restaurant.findAll();
+        expect(restaurantsAfter.length).toEqual(1);
+        expect(restaurantsAfter[0].name).toEqual('Restaurant B');
     });
 })
